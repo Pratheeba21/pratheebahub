556cc9ba9072efb08f2ec86b132f5794e6000390
Pratheeba S <pratheebas887@gmail.com>
2026-06-23 01:53:05 +0530

    LMS
    Signed-off-by: Pratheeba S <pratheebas887@gmail.com>

--- LMS/Backend/server.js
+++ LMS/Backend/server.js
@@ -1953,8 +1953,17 @@ app.use(
 
 app.use(express.json());
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
