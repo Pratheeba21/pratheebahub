626647105a1d63034e20ade0dc5c1b1ab506604d
Pratheeba S <pratheebas887@gmail.com>
2026-06-22 21:19:13 +0530

    LMS
    Signed-off-by: Pratheeba S <pratheebas887@gmail.com>

--- LMS/Frontend/src/Login.jsx
+++ LMS/Frontend/src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 
 
 export default function Login({ onLoginSuccess }) {
